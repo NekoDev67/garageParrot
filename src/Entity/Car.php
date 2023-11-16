961ae321759d5f64cb829a1088f03876e78a8ca3
nekoDev67 <nekodev67@gmail.com>
2023-11-16 21:05:32 +0100

    Cleaning migration & created new table, entity, migration for car

---
+++ src/Entity/Car.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::BLOB)]
+    private $image = null;
+
+    #[ORM\Column(type:"text")]
+    #[Assert\Length(min: 50, max:500)]
+    #[Assert\NotNull()]
+    #[Assert\NotBlank()]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    #[Assert\Positive()]
+    #[Assert\NotNull()]
+    #[Assert\NotBlank()]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $yearOfManufacture = null;
+
+    #[ORM\Column(type:'integer')]
+    #[Assert\NotNull()]
+    #[Assert\NotBlank()]
+    #[Assert\Positive()]
+    private ?int $mileage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getYearOfManufacture(): ?\DateTimeInterface
+    {
+        return $this->yearOfManufacture;
+    }
+
+    public function setYearOfManufacture(\DateTimeInterface $yearOfManufacture): static
+    {
+        $this->yearOfManufacture = $yearOfManufacture;
+
+        return $this;
+    }
+
+    public function getMileage(): ?int
+    {
+        return $this->mileage;
+    }
+
+    public function setMileage(int $mileage): static
+    {
+        $this->mileage = $mileage;
+
+        return $this;
+    }
+}
