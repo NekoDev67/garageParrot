7062455ec809574ea8fbace4187a7c30dff167b9
nekoDev67 <nekodev67@gmail.com>
2023-11-17 12:29:13 +0100

    adding pagination with knp_paginator

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,18 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\CarRepository;
+use Knp\Component\Pager\PaginatorInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
     #[Route('/car', name: 'app_car')]
-    public function index(CarRepository $respository): Response
+    public function index(CarRepository $respository, PaginatorInterface $paginator, Request $request): Response
     {
-        
+        $cars = $paginator->paginate(
+            $respository->findAll(),
+            $request->query->getInt('page', 1), 3
+        );
         return $this->render('pages/car/index.html.twig', [
-            'cars' => $respository->findAll()
+            'cars' =>  $cars
         ]);
     }
 }
